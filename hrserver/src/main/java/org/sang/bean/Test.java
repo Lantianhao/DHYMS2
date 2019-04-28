8152f482b7db0963fe1e36146668b7f90d177aa8
Lantianhao <893911337@qq.com>
2019-04-28 10:45:05 +0800

    init files

---
+++ hrserver/src/main/java/org/sang/bean/Test.java
@@ -0,0 +1,24 @@
+package org.sang.bean;
+
+import java.sql.Timestamp;
+
+public class Test {
+    private int id;
+    private int test;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTest() {
+        return test;
+    }
+
+    public void setOne(int test) {
+        this.test = test;
+    }
+}
